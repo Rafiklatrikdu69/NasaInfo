76663f776d8e857375313456fc5e5341b8ba0a61
Rafiklatrikdu69 <rafik.bouchenna050@email.com>
2024-01-23 19:51:33 +0100

    arrangment style de la page

--- nasa-app/src/app/app.module.ts
+++ nasa-app/src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppComponent } from './app.component';
 import { ImageComponent } from './components/image/image.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule, NativeDateAdapter, DateAdapter, MAT_DATE_FORMATS} from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
     ImageComponent
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MatSlideToggleModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule, MatInputModule, MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule
   ],
   providers: [
     provideClientHydration()
--- nasa-app/src/app/components/image/image.component.ts
+++ nasa-app/src/app/components/image/image.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { ApiService } from '../../modules/module-app/api.service';
 import { apod } from '../../modules/module-app/models/apod';
-import { animate, style, transition, trigger } from '@angular/animations';
 import { date } from '../../modules/module-app/models/date';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
-  styleUrl: './image.component.scss',
+  styleUrls: ['./image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
     trigger('toggleBox', [
      
@@ -23,50 +25,51 @@ import { date } from '../../modules/module-app/models/date';
     ])
   ]
 })
+
 export class ImageComponent {
-  @Input() apod !: apod
-  @Input() tabApi : apod[] = [];
-  @Input() date !: date;
+  @Input() apod!: apod;
+  @Input() tabApi: apod[] = [];
+  @Input() date!: date;
   toggleStates: { id: number; isOpen: boolean }[] = [];
 
-  constructor(private api :ApiService){}
- 
-  ngOnInit(): void {
-  
-   
-  
-  }
-  public handler(e:any){
-console.log(e.target.value)
-this.date = new date();
-      this.date.setDate(e.target.value);
-      this.api.getInfo(this.date.getDate()).subscribe(data=>{
-        console.log(data)
-        let dataParse = JSON.parse(data)
-        for(let i = 0;i<dataParse.length;i++){
-          this.apod = new apod(dataParse[i].copyright,dataParse[i].date,dataParse[i].explanation,dataParse[i].hdurl,dataParse[i].title,dataParse[i].url)
-          console.log(this.apod.getTitre());
-          this.tabApi.push(this.apod)
-        }
-         
-      })   
-      let toggle = document.querySelectorAll('.toggle');
-    
-      for(let i = 1;i<toggle.length;i++){
-        toggle[i].setAttribute("id",i+"")
-       
-        
-      } 
-    
-      
+  constructor(private api: ApiService, private cd: ChangeDetectorRef) {}
+
+  public handler(e: any) {
+    console.log(new Date(e.target.value).getFullYear());
+    console.log(new Date(e.target.value).getDate());
+    console.log(new Date(e.target.value).getMonth()+1);
+
+
+    this.date = new date();
+    this.date.setDate(e.target.value);
+setTimeout(() => {
+    this.api.getInfo(new Date(e.target.value).getFullYear()+"-"+new Date(e.target.value).getMonth()+1+"-"+new Date(e.target.value).getDate()).subscribe(data => {
+      console.log(data);
+      //if(Date.parse(e.target.value)<=d){ 
+      let dataParse = JSON.parse(data);
+      this.tabApi = []; // Réinitialiser le tableau
+      for (let i = 0; i < dataParse.length; i++) {
+        this.apod = new apod(
+          dataParse[i].copyright,
+          dataParse[i].date,
+          dataParse[i].explanation,
+          dataParse[i].hdurl,
+          dataParse[i].title,
+          dataParse[i].url
+        );
+        console.log(this.apod.getTitre());
+        this.tabApi.push(this.apod);
+      }
+      this.cd.markForCheck(); 
+    });
+  },2000)
   }
 
-  isOpen!:any ;
- 
-    toggle(id:number) {
-      this.toggleStates.push({id:id,isOpen:false})
-      this.toggleStates[id].isOpen = !this.toggleStates[id].isOpen;
-      console.log("tourne : "+this.toggleStates[id].isOpen)
- 
-    }
+  isOpen!: any;
+
+  toggle(id: number) {
+    this.toggleStates.push({ id: id, isOpen: false });
+    this.toggleStates[id].isOpen = !this.toggleStates[id].isOpen;
+    console.log('tourne : ' + this.toggleStates[id].isOpen);
+  }
 }
